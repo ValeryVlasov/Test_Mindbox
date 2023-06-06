e17775b448dd4d1b52bffd48e0785023ac72bbce
Валерий Власов <valera.vlasov11@mail.ru>
2023-06-06 18:02:18 +0300

    Circle done

---
+++ Geometry/Circle.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Geometry
+{
+    public class Circle : IFigure
+    {
+        public double Radius { get; }
+
+        public Circle(double radius) {
+            if (radius < 0)
+                throw new ArgumentException("Invalid radius of circle", nameof(radius));
+            Radius = radius;
+        }
+
+        public double GetSquare() { return Math.PI * Math.Pow(Radius, 2); }
+    }
+}
